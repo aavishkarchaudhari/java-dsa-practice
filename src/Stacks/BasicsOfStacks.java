b69522ec10c6501f3b1552fdec5043bf6309f7b6
Aavishkar Chaudhari <chaudhariaavishkar77@gmail.com>
2026-01-04 11:02:50 +0530

    Basics of Stacks

---
+++ src/Stacks/BasicsOfStacks.java
@@ -0,0 +1,26 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class BasicsOfStacks {
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        st.push(6);
+
+        System.out.println(st.peek());
+
+        System.out.println(st);
+        st.pop();
+
+        System.out.println(st);
+
+        System.out.println("size is: " + st.size());
+
+
+    }
+}
