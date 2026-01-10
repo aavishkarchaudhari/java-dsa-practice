43114ebfc8fd6f90ba021e2f207e5e50cfb6e0d6
Aavishkar Chaudhari <chaudhariaavishkar77@gmail.com>
2026-01-10 22:34:33 +0530

    Linked List Implementation of Queue

---
+++ src/Queue/LinkedListImplementationQueue.java
@@ -0,0 +1,86 @@
+package Queue;
+
+public class LinkedListImplementationQueue {
+    public static class Node {
+        int val;
+        Node next;
+
+        Node(int val) {
+            this.val = val;
+        }
+    }
+
+    public static class queueLL {
+        Node head = null;
+        Node tail = null;
+        int size = 0;
+
+        public void add(int x) {
+            Node temp = new Node(x);
+            if (size == 0) {
+                head = tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+
+        public int peek() {
+            if (size == 0) {
+                System.out.println("Queue is Empty!");
+                return -1;
+            }
+            return head.val;
+        }
+
+        public int remove() {
+            if (size == 0) {
+                System.out.println("Queue is Empty!");
+                return -1;
+            }
+            int x = head.val;
+            head = head.next;
+            return x;
+        }
+
+        public void display() {
+            if(size==0){
+                System.out.println("Queue is Empty!");
+                return;
+            }
+            Node temp = head;
+
+            while (temp != null) {
+                System.out.print(temp.val + " ");
+                temp = temp.next;
+            }
+            System.out.println();
+        }
+
+        public boolean isEmpty(){
+            if(size == 0){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        queueLL q = new queueLL();
+        q.display();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.display();
+
+        System.out.println(q.peek());
+        q.remove();
+        q.display();
+        System.out.println(q.peek());
+    }
+}
