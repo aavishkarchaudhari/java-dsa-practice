df92c54591d75f9c523b8234e15b075c13569f70
Aavishkar Chaudhari <chaudhariaavishkar77@gmail.com>
2026-03-23 22:26:42 +0530

    Print Permutations

---
+++ src/Backtracking/PrintPermutations.java
@@ -0,0 +1,22 @@
+package Backtracking;
+
+public class PrintPermutations {
+    public static void printp(String str, String t) {
+        if (str.equals("")) {
+            System.out.print(t + " ");
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            String left = str.substring(0, i);
+            String right = str.substring(i + 1);
+            String rem = left + right;
+            printp(rem, t + ch);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        printp(str, "");
+    }
+}
