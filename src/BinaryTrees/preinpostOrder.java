3ab42f950526eaf6ae32c92739d72487d30da7b1
Aavishkar Chaudhari <chaudhariaavishkar77@gmail.com>
2026-04-04 23:40:38 +0530

    preorder, inorder, and postorder

---
+++ src/BinaryTrees/preinpostOrder.java
@@ -0,0 +1,53 @@
+package BinaryTrees;
+
+public class preinpostOrder {
+
+    static void preorder(Node root) {
+        if (root == null) return;
+
+        System.out.print(root.val + " ");
+        preorder(root.left);
+        preorder(root.right);
+    }
+    static void inorder(Node root) {
+        if (root == null) return;
+
+        inorder(root.left);
+        System.out.print(root.val + " ");
+        inorder(root.right);
+    }
+    static void postorder(Node root) {
+        if (root == null) return;
+
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.val + " ");
+    }
+
+    public static void main(String[] args) {
+
+        // Perfect Binary Tree construction
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        // preorder traversal
+        preorder(root);
+        System.out.println();
+
+        // inorder traversal
+        inorder(root);
+        System.out.println();
+
+        // postorder traversal
+        postorder(root);
+        System.out.println();
+    }
+}
